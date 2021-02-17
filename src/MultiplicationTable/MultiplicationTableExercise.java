349deed879e410ba5b87cb77f802dcecb229007a
rusarwa <rusarwa@gmail.com>
2021-02-17 14:37:17 -0600

    initial commit

---
+++ src/MultiplicationTable/MultiplicationTableExercise.java
@@ -0,0 +1,10 @@
+package MultiplicationTable;
+
+public class MultiplicationTableExercise {
+    public static void main(String[] args) {
+        System.out.println("The Multiplication table of 5");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("5 * " + i + " = " + 5 * i);
+        }
+    }
+}
