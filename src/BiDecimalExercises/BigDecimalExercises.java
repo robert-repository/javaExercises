196f1dcefc2e2a10af50724bdad2a562ea1cfeb4
rusarwa <rusarwa@gmail.com>
2021-03-09 11:11:51 -0600

    BigDecimalExercises

---
+++ src/BiDecimalExercises/BigDecimalExercises.java
@@ -0,0 +1,11 @@
+package BiDecimalExercises;
+
+import java.math.BigDecimal;
+
+public class BigDecimalExercises {
+    public static void main(String[] args) {
+        BigDecimal number1 = new BigDecimal("78.983");
+        BigDecimal number2 = new BigDecimal("876.23");
+        System.out.println(number1.add(number2));
+    }
+}
